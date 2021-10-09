8a8a95657431c0bd22f37ef41dcc23b1479db33d
sumedhreddy90 <sumedhreddykoppula@gmail.com>
2021-10-08 22:30:42 -0400

    removing build and adding .gitignore

--- build/MidTerm/ENPM662/Assem1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sumedh/catkin_ws/src/MidTerm/ENPM662/Assem1
-# Build directory: /home/sumedh/catkin_ws/build/MidTerm/ENPM662/Assem1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/marsRover/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sumedh/catkin_ws/src/marsRover
-# Build directory: /home/sumedh/catkin_ws/build/marsRover
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
